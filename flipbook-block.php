88f7c87f9f5b6a78fade6d4e3d64cc4f48926483
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-03-19 17:11:38 -0400

    Bump to v1.1.0: fix WordPress Plugin Check issues in render.php
    - Add ABSPATH direct access protection to render.php
    - Prefix all variables with flipbook_block_ to satisfy naming conventions
    - Add phpcs:ignore for trusted WP functions in output (get_block_wrapper_attributes)

--- flipbook-block.php
+++ flipbook-block.php
@@ -4,7 +4,7 @@
  * Description:       Display a pdf in a digital flipbook format.
  * Requires at least: 6.6
  * Requires PHP:      7.0
- * Version:           1.0.4
+ * Version:           1.1.0
  * Author:            philhoyt
  * Author URI:        https://philhoyt.com
  * GitHub Plugin URI: https://github.com/philhoyt/flipbook-block
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: philhoyt
 Tags: block, flipbook, pdf, page-flip, viewer
 Requires at least: 6.6
 Tested up to: 7.0
-Stable tag: 1.0.4
+Stable tag: 1.1.0
 Requires PHP: 7.0
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
