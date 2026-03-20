a1d2f38e72aa0f51fbaa086edc910b01e44b76e7
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-03-20 09:15:08 -0400

    Update render.php

--- src/render.php
+++ src/render.php
@@ -13,36 +13,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$flipbook_block_pdf_url            = $attributes['pdfUrl'] ?? '';
-$flipbook_block_id                 = $attributes['blockId'] ?? '';
-$flipbook_block_toolbar            = $attributes['toolbar'] ?? true;
-$flipbook_block_toolbar_always_vis = $attributes['toolbarAlwaysVisible'] ?? true;
-$flipbook_block_enable_fullscreen  = $attributes['enableFullscreen'] ?? true;
-$flipbook_block_enable_download    = $attributes['enableDownload'] ?? false;
-$flipbook_block_download_filename  = $attributes['downloadFilename'] ?? '';
-$flipbook_block_single_page_mode   = $attributes['singlePageMode'] ?? false;
-$flipbook_block_flip_duration      = isset( $attributes['flipDuration'] ) ? (int) $attributes['flipDuration'] : 800;
-$flipbook_block_start_page         = isset( $attributes['startPage'] ) ? (int) $attributes['startPage'] : 1;
+( function ( $attributes ) {
+	$pdf_url            = $attributes['pdfUrl'] ?? '';
+	$id                 = $attributes['blockId'] ?? '';
+	$toolbar            = $attributes['toolbar'] ?? true;
+	$toolbar_always_vis = $attributes['toolbarAlwaysVisible'] ?? true;
+	$enable_fullscreen  = $attributes['enableFullscreen'] ?? true;
+	$enable_download    = $attributes['enableDownload'] ?? false;
+	$download_filename  = $attributes['downloadFilename'] ?? '';
+	$single_page_mode   = $attributes['singlePageMode'] ?? false;
+	$flip_duration      = isset( $attributes['flipDuration'] ) ? (int) $attributes['flipDuration'] : 800;
+	$start_page         = isset( $attributes['startPage'] ) ? (int) $attributes['startPage'] : 1;
 
-$flipbook_block_inner_attrs = sprintf(
-	'id="flipbook-container-%s" data-pdf-url="%s" data-toolbar="%s" data-toolbar-always-visible="%s" data-enable-fullscreen="%s" data-enable-download="%s" data-single-page-mode="%s" data-flip-duration="%d" data-start-page="%d"',
-	esc_attr( $flipbook_block_id ),
-	esc_attr( $flipbook_block_pdf_url ),
-	$flipbook_block_toolbar ? 'true' : 'false',
-	$flipbook_block_toolbar_always_vis ? 'true' : 'false',
-	$flipbook_block_enable_fullscreen ? 'true' : 'false',
-	$flipbook_block_enable_download ? 'true' : 'false',
-	$flipbook_block_single_page_mode ? 'true' : 'false',
-	$flipbook_block_flip_duration,
-	$flipbook_block_start_page
-);
+	$inner_attrs = sprintf(
+		'id="flipbook-container-%s" data-pdf-url="%s" data-toolbar="%s" data-toolbar-always-visible="%s" data-enable-fullscreen="%s" data-enable-download="%s" data-single-page-mode="%s" data-flip-duration="%d" data-start-page="%d"',
+		esc_attr( $id ),
+		esc_attr( $pdf_url ),
+		$toolbar ? 'true' : 'false',
+		$toolbar_always_vis ? 'true' : 'false',
+		$enable_fullscreen ? 'true' : 'false',
+		$enable_download ? 'true' : 'false',
+		$single_page_mode ? 'true' : 'false',
+		$flip_duration,
+		$start_page
+	);
 
-if ( $flipbook_block_download_filename ) {
-	$flipbook_block_inner_attrs .= sprintf( ' data-download-filename="%s"', esc_attr( $flipbook_block_download_filename ) );
-}
+	if ( $download_filename ) {
+		$inner_attrs .= sprintf( ' data-download-filename="%s"', esc_attr( $download_filename ) );
+	}
 
-printf(
-	'<div %s><div %s></div></div>',
-	get_block_wrapper_attributes(), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	$flipbook_block_inner_attrs // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-);
+	printf(
+		'<div %s><div %s></div></div>',
+		get_block_wrapper_attributes(), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		$inner_attrs // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	);
+} )( $attributes );
